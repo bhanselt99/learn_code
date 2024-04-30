5c487175e8a626f0b149614893c12656295c3d8e
Ben <167352628+bhanselt99@users.noreply.github.com>
2024-04-30 15:27:14 -0700

    Update req.py

--- req.py
+++ req.py
@@ -1,6 +1,8 @@
 import requests
 from flask_sqlalchemy import SQLAlchemy
+import random
 
+print(random.uniform(20, 60))
 x = requests.get('https://w3schools.com/python/demopage.htm')
 
 http.Handle("/", http.PageServer(http.Dirextory("./application")))
